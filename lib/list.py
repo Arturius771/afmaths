dc5bb3f8417b4a820396f880fcdaebc398569c60
ArturFoden <fodena@tcd.ie>
2025-11-12 23:42:46 +0100

    some typing

--- lib/list.py
+++ lib/list.py
@@ -2,16 +2,16 @@ import math
 import statistics
 from operation import divide, subtract
 
-def sort(number_list): return sorted(number_list)
-def length(number_list): return len(number_list)
-def sum(number_list): return math.sum(number_list)
-def minimum(number_list): return min(number_list)
-def maximum(number_list): return max(number_list)
-def range(number_list): return subtract(minimum(number_list))(maximum(number_list))
-def mean(number_list): return divide(length(number_list))(sum(number_list))
-def median(number_list): return statistics.median(number_list)
+def sort(number_list: list[float]) -> list[float]: return sorted(number_list)
+def length(number_list: list[float]) -> int: return len(number_list)
+def sum(number_list: list[float]) -> float: return math.sum(number_list)
+def minimum(number_list: list[float]) -> float: return min(number_list)
+def maximum(number_list: list[float]) -> float: return max(number_list)
+def range(number_list: list[float]) -> float: return subtract(minimum(number_list))(maximum(number_list))
+def mean(number_list: list[float]) -> float: return divide(length(number_list))(sum(number_list))
+def median(number_list: list[float]) -> float: return statistics.median(number_list)
 
-def quartiles(number_list: list):
+def quartiles(number_list: list[float]) -> tuple[float, float, float]:
   number_list = sorted(number_list)
   q1_index = int(math.ceil(length(number_list) * 0.25))
   q1_result = number_list[q1_index - 1]
@@ -21,7 +21,7 @@ def quartiles(number_list: list):
   
   return q1_result, q3_result, iqr_result
 
-def dataplotter(number_list):
+def dataplotter(number_list: list[float]) -> None:
   ##MU123
   """
   Runs some useful functions on a provided list
