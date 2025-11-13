264c7e799159ecf3d5833ada4a4b9e230df85429
ArturFoden <fodena@tcd.ie>
2025-11-13 01:02:15 +0100

    adjustments

--- lib/graph.py
+++ lib/graph.py
@@ -1,11 +1,11 @@
 from operation import divide, multiply, subtract
 
-def gradient(x1, y1, x2, y2):
+def gradient(x1: float, y1: float, x2: float, y2: float) -> float:
     """Calculates the slope between two points"""
   ##https://www.bbc.co.uk/bitesize/topics/zvhs34j/articles/z4ctng8
     return divide(subtract(x1)(x2))(subtract(y1)(y2))
 
-def equation_of_line(x1, y1, x2, y2):
+def equation_of_line(x1: float, y1: float, x2: float, y2: float) -> tuple[float, float]:
   ##m = gradient
   m = gradient(x1, y1, x2, y2)
   ##b = y intercept when x = 0
--- lib/physics.py
+++ lib/physics.py
@@ -47,20 +47,19 @@ def watts_to_decibel_milliwatts(power_in_watts: float) -> float: return decibels
 
 def decibels(power1: float): return lambda power2: math.log(power1/power2, 10)
 
-def hohmann_transfer(initial_altitude_metres: int, target_altitude_metres: int, initial_body_radius: int = 6378000):
+def hohmann_transfer(initial_altitude_metres: int, target_altitude_metres: int, initial_body_radius: int = 6378000, G_M: float = 3.986005e+14):
     # www.braeunig.us/space/problem.htm#4.19
 
     r_a = initial_altitude_metres + initial_body_radius
     r_b = target_altitude_metres + initial_body_radius
-    G_M = 3.986005e+14 # for Earth
 
     semi_major_axis_transfer_ellipse = (r_a + r_b) / 2
     initial_velocity = square_root(G_M / r_a) # TODO extract into a Vis Viva equation. 
     final_velocity = square_root(G_M / r_b)
-    velocity_on_orbit_at_initial_orbit = square_root(G_M * ((2/r_a) - (1/semi_major_axis_transfer_ellipse)))
-    velocity_on_orbit_at_final_orbit = square_root(G_M * ((2/r_b) - (1/semi_major_axis_transfer_ellipse)))
-    initial_velocity_change = velocity_on_orbit_at_initial_orbit - initial_velocity
-    final_velocity_change = final_velocity - velocity_on_orbit_at_final_orbit
+    velocity_at_transfer_periapsis = square_root(G_M * ((2/r_a) - (1/semi_major_axis_transfer_ellipse)))
+    velocity_at_transfer_apoapsis = square_root(G_M * ((2/r_b) - (1/semi_major_axis_transfer_ellipse)))
+    initial_velocity_change = velocity_at_transfer_periapsis - initial_velocity
+    final_velocity_change = final_velocity - velocity_at_transfer_apoapsis
     total_velocity_change = initial_velocity_change + final_velocity_change
 
     return total_velocity_change
