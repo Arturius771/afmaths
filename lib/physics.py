a2f4e6dcc3dd00691cb1b94ed090be2f8746b5d5
Artur Foden <adfoden@gmail.com>
2025-12-16 14:58:47 +0000

    Move hohmann transfer

--- lib/physics.py
+++ lib/physics.py
@@ -6,7 +6,6 @@ SPEED_OF_LIGHT_METRES_PER_SECONDS = 299792458
 PLANCK_CONSTANT = multiply(6.62607004)(exponentiate(-34)(10))  
 GRAVITATIONAL_CONSTANT = 6.67430e-11
 
-
 def radiowave_relative_power_distances(distance1, distance2):
   ##TM255 Block 1
   distance_ratio = divide(distance1)(distance2)
@@ -46,23 +45,6 @@ def dynamic_pressure(fluid_mass_density, flow_speed): return (fluid_mass_density
 def watts_to_decibel_milliwatts(power_in_watts: float) -> float: return decibels(power_in_watts)(.001)
 
 def decibels(power1: float): return lambda power2: math.log(power1/power2, 10)
-
-def hohmann_transfer(initial_altitude_metres: int, target_altitude_metres: int, initial_body_radius: int = 6378000, G_M: float = 3.986005e+14):
-    # www.braeunig.us/space/problem.htm#4.19
-
-    r_a = initial_altitude_metres + initial_body_radius
-    r_b = target_altitude_metres + initial_body_radius
-
-    semi_major_axis_transfer_ellipse = (r_a + r_b) / 2
-    initial_velocity = square_root(G_M / r_a) # TODO extract into a Vis Viva equation. 
-    final_velocity = square_root(G_M / r_b)
-    velocity_at_transfer_periapsis = square_root(G_M * ((2/r_a) - (1/semi_major_axis_transfer_ellipse)))
-    velocity_at_transfer_apoapsis = square_root(G_M * ((2/r_b) - (1/semi_major_axis_transfer_ellipse)))
-    initial_velocity_change = velocity_at_transfer_periapsis - initial_velocity
-    final_velocity_change = final_velocity - velocity_at_transfer_apoapsis
-    total_velocity_change = initial_velocity_change + final_velocity_change
-
-    return total_velocity_change
 
 def flux_density(luminosity, distance_metres):
   return inverse_square_law(luminosity, distance_metres)
---
+++ lib/space_engineering.py
@@ -0,0 +1,24 @@
+from lib.operation import square_root
+
+
+def hohmann_transfer(initial_altitude_metres: int, target_altitude_metres: int, initial_body_radius: int = 6378000):
+    # www.braeunig.us/space/problem.htm#4.19
+
+    r_a = initial_altitude_metres + initial_body_radius
+    r_b = target_altitude_metres + initial_body_radius
+    G_M = 3.986005e+14 # for Earth
+
+    semi_major_axis_transfer_ellipse = (r_a + r_b) / 2
+    initial_velocity = square_root(G_M / r_a)
+    final_velocity = square_root(G_M / r_b)
+    velocity_on_orbit_at_initial_orbit = square_root(G_M * ((2/r_a) - (1/semi_major_axis_transfer_ellipse)))
+    velocity_on_orbit_at_final_orbit = square_root(G_M * ((2/r_b) - (1/semi_major_axis_transfer_ellipse)))
+    initial_velocity_change = velocity_on_orbit_at_initial_orbit - initial_velocity
+    final_velocity_change = final_velocity - velocity_on_orbit_at_final_orbit
+    total_velocity_change = initial_velocity_change + final_velocity_change
+
+    return total_velocity_change
+
+# TODO: Vis Viva
+# TODO: Orbital velocity
+# TODO: FST 1 equations
