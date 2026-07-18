f66da373a324607bd0979a4badc1285467f1c7a7
Artur Foden <adfoden@gmail.com>
2026-07-18 12:45:58 +0100

    v2.1.0

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="afmaths",
-    version="2.0.4",
+    version="2.1.0",
     package_dir={"": "lib"},
     packages=find_packages(where="lib"),
     author="Artur Foden",
-    description="A simple package of math functions.",
+    description="A suite of mathematical functions",
     long_description=Path("readme.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
     url="https://github.com/Arturius771/afmaths",
