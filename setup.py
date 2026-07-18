9fd2d6dc6e4e7c4d298d8a8a9d464cb2be1eb717
Artur Foden <adfoden@gmail.com>
2026-07-18 12:57:47 +0100

    v2.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="afmaths",
-    version="2.1.0",
+    version="2.1.1",
     package_dir={"": "lib"},
     packages=find_packages(where="lib"),
     author="Artur Foden",
